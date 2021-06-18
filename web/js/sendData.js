fb4df525b33469602c1c7a2024b4b84ebd06323f
M-Shahjahan <78802425+M-Shahjahan@users.noreply.github.com>
2021-06-18 15:26:34 +0500

    Ajax second attempt errors resolved

--- web/js/sendData.js
+++ web/js/sendData.js
@@ -4,33 +4,60 @@ $(document).ready(function () {
 
         event.preventDefault();
         var form=$(this);
-
+        console.log('1\n2\n3')
         document.getElementById("nominate").disabled = true;
         var data=form.serializeArray();
+        var values=["Name","Email","School","Address","Connection","Contact","Position","Email Address","Other Info",""];
+        let finalData = [
+           data[1].value,
+           data[2].value,
+           data[3].value,
+           data[4].value,
+           data[6].value,
+           data[7].value,
+           data[8].value,
+           data[9].value,
+           data[10].value,
+           data[11].value
+       ];
+        console.log(finalData);
+        for(var index=0;index<10;++index){
+
+            if(finalData[index]==""){
+                if(values[index]==""){
+                    document.getElementsByClassName('help-block')[index].innerHTML="Please confirm you are not a bot.";
+                }
+                else{
+                    document.getElementsByClassName('help-block')[index].innerHTML=values[index]+" cannot be blank.";
+                }
+            }
+
+        }
         $.ajax({
-            url: 'https://localhost/basic/web/?r=site/submit',
-            type:'post',
+            url: 'localhost/basic/web/?r=site/submit',
+            type:'POST',
             data: data,
             dataType:'json'
             }
         ).done(function (response) {
+            console.log(response);
             if(response==1){
 
-                document.getElementById('flash_text').innerText="Thank You! You have successfully submitted a Nomination";
+                document.getElementById('flash_text').innerText="Thank You! You have successfully submitted a nomination";
                 document.getElementById('flash_text').style.color="#092864";
                 document.getElementById("nominate").disabled = false;
                 document.getElementById('nominationform-recaptcha').value="";
-                document.getElementById('nominationForm').reset();
                 $("#nominationForm").trigger("reset");
                 grecaptcha.reset();
             }
             else if(response==0){
-                document.getElementById('flash_text').innerText="Please Fill the Missing Fields";
+                document.getElementById('flash_text').innerText="Please fill the missing fields";
                 document.getElementById('flash_text').style.color="white";
                 document.getElementById("nominate").disabled = false;
                 document.getElementById('nominationform-recaptcha').value="";
                 grecaptcha.reset();
             }
+            document.getElementById("nominate").disabled = false;
         })
     });
 })
