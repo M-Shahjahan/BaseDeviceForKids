3eac9b600555567c2c88e88d23876a8ea51dec3e
M-Shahjahan <78802425+M-Shahjahan@users.noreply.github.com>
2021-06-29 15:43:26 +0500

    Database Configured

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=deviceforkids',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=72.47.208.54;dbname=expressp_dfkids',
+    'username' => 'expressp_devicesforkids',
+    'password' => 'rYB9m?gGjAaf',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
